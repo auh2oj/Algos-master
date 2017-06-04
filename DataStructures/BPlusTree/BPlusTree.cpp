3c031eb2ec8c8d2b896e06f5e00d5a3848f96484
Joshua Goldwasser <spearfish@berkeley.edu>
2017-06-04 11:28:55 -0700

    cleaned things up

--- DataStructures/BPlusTree/BPlusTree.cpp
+++ DataStructures/BPlusTree/BPlusTree.cpp
@@ -14,51 +14,38 @@
 
 using namespace std;
 
-struct BPlusEntry;
-
 struct BPlusNode {
 	
 	int d;
 	int num_entries;
 	BPlusNode *first_child;
-//	pair<int, BPlusNode*> *entries;
 	vector<pair<int, BPlusNode*> > entries;
 	bool is_leaf;
 	
 };
 
-//struct BPlusEntry {
-//	int data;
-//	BPlusNode *child;
-//};
 
 BPlusNode *new_node(int order, bool is_leaf) {
 	BPlusNode *node = new BPlusNode;
 	node->d = order;
-//	node->entries = new pair<int, BPlusNode*> [order];
 	node->is_leaf = is_leaf;
 	node->num_entries = 0;
 	return node;
 }
 
-//BPlusEntry *new_entry(int data) {
-//	BPlusEntry *entry = new BPlusEntry;
-//	entry->data = data;
-//}
-
 bool entry_comparator(pair<int, BPlusNode*> i, pair<int, BPlusNode*> j) {
 	return i.first < j.first;
 }
 
-//pair<int, BPlusNode*> split_leaf_node(pair<int, BPlusNode*> entry, BPlusNode *node) {
-//	int order = node->d;
-//	BPlusNode *new_leaf = new_node(order, true);
-//	pair<int, BPlusNode*> node_entries = new pair<int, BPlusNode*> [order + 1];
-//	for (int i = 0; i < order; i++) {
-//		node_entries[i] = node->entries[i];
-//	}
-//	node_entries[order]
-//}
+pair<int, BPlusNode*> split_leaf_node(pair<int, BPlusNode*> entry, BPlusNode *node) {
+	int order = node->d;
+	BPlusNode *new_leaf = new_node(order, true);
+	pair<int, BPlusNode*> node_entries = new pair<int, BPlusNode*> [order + 1];
+	for (int i = 0; i < order; i++) {
+		node_entries[i] = node->entries[i];
+	}
+	node_entries[order]
+}
 
 pair<int, BPlusNode*> *insert_leaf_entry(pair<int, BPlusNode*> entry, BPlusNode *node) {
 	if (node->num_entries == 2*node->d) {
@@ -66,10 +53,8 @@ pair<int, BPlusNode*> *insert_leaf_entry(pair<int, BPlusNode*> entry, BPlusNode 
 		return &entry;
 	} else {
 		//insert entry as normal
-//		node->entries[node->num_entries++] = entry;
 		node->entries.push_back(entry);
 		node->num_entries++;
-//		sort(node->entries, node->entries + node->num_entries, entry_comparator);
 		sort(node->entries.begin(), node->entries.end(), entry_comparator);
 		return nullptr;
 	}
@@ -90,12 +75,8 @@ int main() {
 	
 	for (int i = 0; i < 2*root->d; i++) {
 		pair<int, BPlusNode*> new_entry = make_pair(i, nullptr);
-//		root->entries[root->num_entries] = new_entry;
-//		root->num_entries++;
 		insert_leaf_entry(new_entry, root);
 	}
-	
-//	sort(root->entries, root->entries + 2*root->d, entry_comparator);
 	
 	cout << "d is: " << root->d;
 	cout << "\n\n";
